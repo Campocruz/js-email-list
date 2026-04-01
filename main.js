9f69605c759852e08f29a3cdb9a7f26421d50f64
Campocruz <cosmacampo93@yahoo.it>
2026-04-01 12:22:48 +0200

    test ajax

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log('work');
+const api_mail_endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+fetch(api_mail_endpoint)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.response)
+  })
