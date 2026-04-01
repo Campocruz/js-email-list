7ccc2e4a589c09bed2b7574c51bf956c3fc7b4d7
Campocruz <cosmacampo93@yahoo.it>
2026-04-01 13:08:21 +0200

    generate new list after click botton

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
+// Define the number of item list
 const listItems = 10;
-
+// Define the URL endpoint for API request 
 const api_mail_endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+// Select DOME node to print the email list
 const emailListEl = document.getElementById('email_list');
+const btnEl = document.getElementById('btnGenerate')
 
 for (let i = 0; i < listItems; i++) {
-  fetch(api_mail_endpoint)
+  getEmail(api_mail_endpoint);
+}
+
+btnEl.addEventListener('click', () => {
+  emailListEl.innerHTML = '';
+  for (let i = 0; i < listItems; i++) {
+    getEmail(api_mail_endpoint);
+  }
+})
+
+
+
+function getEmail(endpoint) {
+  fetch(endpoint)
     .then(response => response.json())
     .then(data => {
-      const liEl = document.createElement('li');
-      liEl.innerHTML = data.response;
-      emailListEl.appendChild(liEl);
+      printInList(data.response)
     })
+}
+
+function printInList(email) {
+  const liEl = document.createElement('li');
+  liEl.innerHTML = email;
+  emailListEl.appendChild(liEl);
 }
